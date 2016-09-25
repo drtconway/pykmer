70ac5947e313474ae039f0db41b5d99d563d0b8d
Tom Conway <tom@Toms-MacBook-Pro.local>
2016-09-25 20:44:41 +1000

    Bugfixes

--- pykmer/__init__.py
+++ pykmer/__init__.py
@@ -1,2 +1,2 @@
 from basics import kmer, render, rc, ham, lcp
-from basics import readFasta, readFastq
+from file import readFasta, readFastq
--- pykmer/kset.py
+++ pykmer/kset.py
@@ -1,15 +1,16 @@
 from codec8 import encode, decode
 
-def write(k, xs, f):
-    bs = bytearray(encode(k))
-    f.write(bs)
-    p = 0
-    for x in xs:
-        assert p < x
-        d = x - p
-        bs = bytearray(encode(x))
+def write(k, xs, nm):
+    with open(nm, "wb") as f:
+        bs = bytearray(encode(k))
         f.write(bs)
-        p = x
+        p = 0
+        for x in xs:
+            assert p < x
+            d = x - p
+            bs = bytearray(encode(d))
+            f.write(bs)
+            p = x
 
 def read0(itr):
     x = 0
@@ -20,8 +21,10 @@ def read0(itr):
         except StopIteration:
             return
 
-def read(f):
+def read(nm):
+    f = open(nm, "rb")
     bs = bytearray(f.read())
+    f.close()
     itr = bs.__iter__()
     k = decode(itr)
     return (k, read0(itr))
