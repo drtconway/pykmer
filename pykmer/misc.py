0f27c97b5570de7ebe4fb6d8fc1e8344b11e6474
Tom Conway <tconway@au1.ibm.com>
2016-12-20 10:21:43 +1100

    Move the suffix array class into misc.

--- pykmer/misc.py
+++ pykmer/misc.py
@@ -190,3 +190,133 @@ class heap:
                 c = 2*p
             else:
                 break
+
+class suffa:
+    """
+    This class implements a suffix array over a string.
+
+    The suffix sorting is done using builtin sort (i.e. regular comparison
+    based sorting) so is likely to perform poorly in some cases for
+    large inputs.
+    """
+    def __init__(self, seq):
+        """
+        Create a new suffix array object based on `seq`.
+        """
+        self.seq = seq
+        self.suffs = [i for i in xrange(len(seq))]
+        self.suffs.sort(self.cmp)
+
+    def __len__(self):
+        """
+        Return the number of suffixes.
+        """
+        return len(self.suffs)
+
+    def suff(self, i):
+        """
+        Return the suffix number (i.e. starting position in the original
+        string) of the `i`th suffix.
+        """
+        return self.suffs[i]
+
+    def __getitem__(self, i):
+        """
+        Return the `i`th suffix as a string.
+        """
+        p = self.suffs[i]
+        return self.seq[p:]
+
+    def lcp(self, s):
+        """
+        Locate the suffix sharing the longest common prefix with `s`,
+        and return a tuple containing the length of the common prefix
+        and the suffix.
+        """
+        l = 0
+        h = len(self.suffs)
+        while l < h:
+            m = (l + h) // 2
+            c = self.cmp_str(s, self.suffs[m])
+            if c == 0:
+                return len(s)
+            if c < 0:
+                h = m - 1
+            else:
+                l = m + 1
+        if l == 0:
+            return (self.lcp_suff(s, self.suffs[l]), self.suffs[l])
+        if l == len(self.suffs):
+            return (self.lcp_suff(s, self.suffs[l - 1]), self.suffs[l - 1])
+        r1 = self.lcp_suff(s, self.suffs[l - 1])
+        r2 = self.lcp_suff(s, self.suffs[l])
+        if r1 >= r2:
+            return (r1, self.suffs[l - 1])
+        else:
+            return (r1, self.suffs[l])
+
+    def cmp(self, a, b):
+        i = a
+        j = b
+        while i < len(self.seq) and j < len(self.seq):
+            if self.seq[i] < self.seq[j]:
+                return -1
+            if self.seq[i] > self.seq[j]:
+                return 1
+            i += 1
+            j += 1
+        if i < len(self.seq):
+            return -1
+        else:
+            return 1
+
+    def cmp_str(self, s, b):
+        i = 0
+        j = b
+        while i < len(s) and j < len(self.seq):
+            if s[i] < self.seq[j]:
+                return -1
+            if s[i] > self.seq[j]:
+                return 1
+            i += 1
+            j += 1
+        if i < len(s):
+            return -1
+        elif j < len(self.seq):
+            return 1
+        else:
+            return 0
+
+    def lcp_suff(self, s, b):
+        i = 0
+        j = b
+        while i < len(s) and j < len(self.seq):
+            if s[i] != self.seq[j]:
+                return i
+            i += 1
+            j += 1
+        return i
+
+    def lcp(self, s):
+        l = 0
+        h = len(self.suffs)
+        while l < h:
+            m = (l + h) // 2
+            c = self.cmp_str(s, self.suffs[m])
+            if c == 0:
+                return len(s)
+            if c < 0:
+                h = m - 1
+            else:
+                l = m + 1
+        if l == 0:
+            return (self.lcp_suff(s, self.suffs[l]), self.suffs[l])
+        if l == len(self.suffs):
+            return (self.lcp_suff(s, self.suffs[l - 1]), self.suffs[l - 1])
+        r1 = self.lcp_suff(s, self.suffs[l - 1])
+        r2 = self.lcp_suff(s, self.suffs[l])
+        if r1 >= r2:
+            return (r1, self.suffs[l - 1])
+        else:
+            return (r1, self.suffs[l])
+
