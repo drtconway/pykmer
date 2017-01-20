59f85fa9f36c576ea35c19b96b0556f0df3e9132
Tom Conway <tconway@au1.ibm.com>
2017-01-20 12:45:48 +1100

    Enrich smart file opening.

--- pykmer/file.py
+++ pykmer/file.py
@@ -45,14 +45,39 @@ def readFastq(file):
     if grp == 4:
         yield tuple(grp)
 
-def openFile(fn):
-    if fn == "-":
-        return sys.stdin
-    if fn.endswith(".gz"):
-        p = subprocess.Popen(['gunzip', '-c', fn], stdout=subprocess.PIPE)
-        return p.stdout
-    if fn.endswith(".bz2"):
-        p = subprocess.Popen(['bunzip2', '-c', fn], stdout=subprocess.PIPE)
-        return p.stdout
-    return open(fn, 'rb')
+def openFile(fn, mode='r'):
+    """
+    Open a file "cleverly".
+
+    If the file name ends with ".gz" or ".bz2", it is compressed
+    or uncompressed on the fly (according to the mode).
+
+    In read mode (mode='r'), the filename '-' is interpreted as stdin.
+
+    In write mode (mode='w'), the filename '-' is interpreted as stdout.
+    """
+    if mode == 'r':
+        if fn == "-":
+            return sys.stdin
+        if fn.endswith(".gz"):
+            p = subprocess.Popen(['gunzip', '-c', fn],
+                                 stdout=subprocess.PIPE)
+            return p.stdout
+        if fn.endswith(".bz2"):
+            p = subprocess.Popen(['bunzip2', '-c', fn],
+                                 stdout=subprocess.PIPE)
+    if mode == 'w':
+        if fn == "-":
+            return sys.stdout
+        if fn.endswith(".gz"):
+            p = subprocess.Popen(['gzip', '-9', '-', '>', fn],
+                                 stdin=subprocess.PIPE,
+                                 shell=True)
+            return p.stdin
+        if fn.endswith(".bz2"):
+            p = subprocess.Popen(['bzip2', '-9', '-', '>', fn],
+                                 stdout=subprocess.PIPE,
+                                 shell=True)
+        return p.stdin
+    return open(fn, mode)
 
